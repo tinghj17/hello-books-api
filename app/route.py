a5396fe803560b9f91fe8d933ed7ce3add6cd09b
tinghj17 <tinghj17@gmail.com>
2022-04-25 22:29:34 -0700

    check out one book

--- app/route.py
+++ app/route.py
@@ -1,5 +1,5 @@
 # blueprint is a Flask class that provides a pattern for grouping related routes (endpoints)
-from flask import Blueprint,jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -13,11 +13,30 @@ books = [
     Book(3,"book3", "description3")
 ]
 
-all_books_bp = Blueprint("all_books", __name__)
+all_books_bp = Blueprint("all_books", __name__, url_prefix="/show-all-books")
 
+def valid_input(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message": f"oops, {book_id} is in valid "}, 400))
+    for book in books:
+        if book_id == book.id:
+            return book
+    abort(make_response({"message": f"{book_id} is not found"}, 404))
+    
+
+@all_books_bp.route("/<book_id>", methods=["GET"])
+def get_one_book(book_id):
+    book = valid_input(book_id)
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+            }
 
 # creating an endpoint
-@all_books_bp.route("/show-all-books", methods=["GET"])
+@all_books_bp.route("", methods=["GET"])
 def demonstrate_all_books():
     result_all_books = []
     for book in books:
